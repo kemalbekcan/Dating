22293e2552fdbb14c164edff9c14f98c85705b0a
kemalbekcan <kemalbekcan@gmail.com>
2022-05-02 17:10:19 +0300

    backend router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,12 +3,18 @@ var express = require("express");
 var router = express.Router();
 
 router.post("/create", function (req, res) {
-  console.log('req', req.body)
-  // models.User.create({
-  //   username: req.body.username,
-  // }).then(function () {
-  //   res.redirect("/");
-  // }).cath;
+  console.log("req", req.body);
+  models.User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+  })
+    .then(function () {
+      console.log("sended");
+    })
+    .cath((error) => {
+      console.log("error");
+    });
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-var cors = require('cors')
-// const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+var cors = require("cors");
+const bodyParser = require("body-parser");
 
-var users = require('./routes/users')
+var users = require("./routes/users");
 
-app.use(cors())
+app.use(cors());
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', users);
+app.use("/", users);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
-})
+  console.log(`Example app listening on port ${3000}`);
+});
